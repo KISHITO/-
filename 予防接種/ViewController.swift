2f45d573c0a9a2aab648c9cc50e7c7191c37ce21
池田俊輝 <toshi0507@dn0a0fbbd7.sunet>
2017-08-17 10:47:02 -0700

    Initial Commit

---
+++ 予防接種/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  予防接種
+//
+//  Created by 池田俊輝 on 2017/08/17.
+//  Copyright © 2017年 池田俊輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
